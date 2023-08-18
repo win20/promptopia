a4758ee20fc10c1550fc4726b8f0c48d0f31db07
Win Barua <win.barua2@gmail.com>
2023-08-18 20:31:52 +0100

    Homepage feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+'use client';
+
+import { useState, useEffect } from 'react'
+
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({data, handleTagClick}) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
+}
 
 const Feed = () => {
+
+  const [searchText, setsearchText] = useState('');
+  const [posts, setposts] = useState([]);
+
+  const handleSearchChange = (e) => {
+
+  };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+      setposts(data);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form action="" className='relative w-full flex-center'>
+        <input type="text" placeholder='Search for a tag or a username' value={searchText} onChange={handleSearchChange} className='search_input peer' required />
+
+      </form>
+
+      <PromptCardList
+        data={posts}
+        handleTagClick={() => {}}
+      />
+    </section>
   )
 }
 
