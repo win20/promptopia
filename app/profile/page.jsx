0b578aea6a6078a1c0016fa1058817976e4e4e7a
Win Barua <win.barua2@gmail.com>
2023-08-19 14:06:51 +0100

    Delete post functionality

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -28,7 +28,21 @@ const MyProfile = () => {
   };
 
   const handleDelete = async (post) => {
+    const hasConfirmed = confirm('Are you sure you want to delete this prompt');
 
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: 'DELETE',
+        });
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+
+      }
+    }
   };
 
   console.log(posts);
