7d3c0077a973be74dc5291ca24dbb221713940e3
Win Barua <win.barua2@gmail.com>
2023-08-19 14:01:01 +0100

    Update prompt

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -43,3 +43,19 @@ export const PATCH = async (request, { params }) => {
     });
   }
 }
+
+export const DELETE = async (request, { params }) => {
+  try {
+    await connectToDB();
+    await Prompt.findByIdAndRemove(params.id);
+
+    return new Response('Prompt deleted successfully', {
+      status: 200,
+    });
+
+  } catch (error) {
+    return new Response('Failed to delete prompt', {
+      status: 500,
+    });
+  }
+}
