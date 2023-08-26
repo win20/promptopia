9840a0450f80daf92168a566ff0a7f8e49677176
Win Barua <win.barua2@gmail.com>
2023-08-26 16:56:06 +0100

    test

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -32,7 +32,7 @@ const Nav = () => {
           className='object-contain'
         />
 
-        <p className='logo_text'>Promptopiaaa</p>
+        <p className='logo_text'>Promptopiaa</p>
       </Link>
 
       {/* Desktop Navigation */}
