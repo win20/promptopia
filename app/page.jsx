eb86af9cd8f46e628b79eae90daaa2d51d6db638
Win Barua <win.barua2@gmail.com>
2023-08-27 16:54:36 +0100

    Add favicon

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import Feed from '@components/Feed';
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
+      <link rel="icon" href="/assets/images/logo.svg" />
       <h1 className='head_text text-center'>
         Discover & Share
         <br className='md:hidden' />
